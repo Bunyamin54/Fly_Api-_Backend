e4526636d1caec85fd9f10d62d78588b29557182
Bunyamin Genc <112567741+Bunyamin54@users.noreply.github.com>
2023-10-19 21:30:20 +0200

    isLogin

--- FlightAPI/src/middlewares/permissions.js
+++ FlightAPI/src/middlewares/permissions.js
@@ -4,41 +4,36 @@
 ------------------------------------------------------- */
 // Middleware: permissions
 
-module.exports = {
+module.exports =   {
 
-    isLogin: (req, res, next) => {
 
-        return next()
-        
-        if (req.user) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login.')
-        }
+    isLogin : (req, res, next) => {
+    
+
+        isLogin: (req, res, next) => {
+
+            if (req.user) {
+                next()
+            } else {
+                res.errorStatusCode = 403
+                throw new Error('NoPermission: You must login.')
+            }
+        },
     },
+    isStaff : (req, res, next) =>{
+        isLogin: (req, res, next) => {
 
-    isAdmin: (req, res, next) => {
+            if (req.user && req.user.isStaff) {
+                next()
+            } else {
+                res.errorStatusCode = 403
+                throw new Error('NoPermission: You must login and to be Staff.')
+            }
+        },
 
-        return next()
-        
-        if (req.user && req.user.isAdmin) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Admin.')
-        }
     },
+    isAdmin :(req, res, next) =>{
 
-    isStaff: (req, res, next) => {
 
-        return next()
-        
-        if (req.user && (req.user.isAdmin || req.user.isStaff)) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Staff.')
-        }
     },
 }
